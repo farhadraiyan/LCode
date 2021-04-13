5fca255230f81daa450020d97b945fcff828be73
Farhad <farhadhossaintoronto@gmail.com>
2021-04-13 02:58:06 -0400

    problem 27 solved

--- src/main/Driver.java
+++ src/main/Driver.java
@@ -3,8 +3,8 @@ package main;
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("test");
+		Problems P =new Problems();
+		
 		
 	}
 
